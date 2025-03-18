fb89ad6367a7519fb6f0fa697485cb9e143b97ef
Maulvi Maulana <maulvizm@MacBook-Pro-Maulvi.local>
2025-03-18 22:50:21 +0700

    fix: add testcase parameter to run.sh

--- src/open-mpi/run.sh
+++ src/open-mpi/run.sh
@@ -1,9 +1,15 @@
 if [ -z "$1" ]; then
-    echo "Usage: $0 <number_of_nodes>"
+    echo "Usage: $0 <number_of_nodes> <test_num>"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Usage: $0 <number_of_nodes> <test_num>"
     exit 1
 fi
 
 N=$1 # Number of nodes
+TESTCASE=$2
 
-docker exec -it node1 sh -c "mpicc mpi.c -o mpi"
-docker exec -it node1 sh -c "mpirun --allow-run-as-root -np $N --hostfile /mpi/hostfile mpi"
+docker exec -it node1 sh -c "mpicc mpi.c -o mpi -lm"
+docker exec -it node1 sh -c "mpirun --allow-run-as-root -np $N --hostfile /mpi/hostfile mpi < test_case/case$TESTCASE.txt > out-$TESTCASE.txt"
