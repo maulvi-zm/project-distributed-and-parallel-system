9cc00dda09b715b26535fc0a9c1567bc19e9952b
Maulvi Maulana <maulvizm@MacBook-Pro-Maulvi.local>
2025-03-18 22:36:57 +0700

    fix: code filename in run.sh

--- src/open-mpi/run.sh
+++ src/open-mpi/run.sh
@@ -5,5 +5,5 @@ fi
 
 N=$1 # Number of nodes
 
-docker exec -it node1 sh -c "mpicc hello.c -o mpi"
+docker exec -it node1 sh -c "mpicc mpi.c -o mpi"
 docker exec -it node1 sh -c "mpirun --allow-run-as-root -np $N --hostfile /mpi/hostfile mpi"
